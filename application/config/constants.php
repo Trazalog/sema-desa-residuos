f86b89d4cbe663afb93f6eeae2dced114f759286
fernando <fguardia@trazalog.com>
2019-08-29 04:19:23 -0300

    Cambio Nombre Proyecto

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,8 +85,8 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 //Nombre de Prouyecto
-define('MNOM', 'ADM');
-define('NOM', 'Admin LTE');
+define('MNOM', 'RSD');
+define('NOM', 'Residuos');
 
 //Vista por Defecto
 define('DEF_VIEW','test');
