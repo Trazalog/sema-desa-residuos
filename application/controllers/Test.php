f881473e70669e0177d8383a6e7ea59392a281b4
FLeivaTraza <fleiva@trazalog.com>
2019-09-03 04:07:41 -0300

    SETUP | Base del Proyecto

---
+++ application/controllers/Test.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model(FRM.'Forms');
+    }
+
+    public function index()
+    {
+        $data['forms'] = $this->Forms->listado();
+        $this->load->view('test', $data);
+    }
+}
