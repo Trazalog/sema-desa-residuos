2f586d9465c6f24609a523385696fe58df5e2197
Jose Roberto <jose.roberto.basanes@hotmail.com>
2020-01-15 12:56:16 -0300

    CONTROLLER / Select |Mocks|

--- application/controllers/general/RegistrarCi.php
+++ application/controllers/general/RegistrarCi.php
@@ -5,25 +5,29 @@ class RegistrarCi extends CI_Controller {
     function __construct(){
 
       parent::__construct();
-      //$this->load->helper('choferes1_helper');
-      //$this->load->helper('choferes1_helper');
+      $this->load->helper('choferes_helper');
+      $this->load->helper('vehiculos_helper');
+      $this->load->helper('tipo_residuos_helper');
 
-      //$this->load->model('general/Choferes');
-      //$this->load->model('general/Choferes');
+      $this->load->model('general/Choferes');
+      $this->load->model('general/Vehiculos');
+      $this->load->model('general/TipoResiduos');
    }
 
    function registrarCi()
    {
-       //$data['Chofer'] = $this->Chofer->obtener();
-       //$data['Chofer'] = $this->Chofer->obtener();
-       $this->load->view('layout/registrar_circuitos');
+       $data['Chofer'] = $this->Chofer->obtener();
+       $data['Vehiculo'] = $this->Vehiculo->obtener();
+       $data['tipoResiduos'] = $this->tipoResiduos->obtener();
+       $this->load->view('layout/registrar_circuitos', $data);
    }
    
    function templateRci()
    {
-       //$data['Chofer'] = $this->Chofer->obtener();
-       //$data['Chofer'] = $this->Chofer->obtener();
-       $this->load->view('layout/registrar_circuitos');
+       $data['Chofer'] = $this->Chofer->obtener();
+       $data['Vehiculo'] = $this->Vehiculo->obtener();
+       $data['tipoResiduos'] = $this->tipoResiduos->obtener();
+       $this->load->view('layout/registrar_circuitos', $data);
        
    }
 }
