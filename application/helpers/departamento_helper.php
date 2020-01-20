17184248d97084ae1b4ee96a812a83135dc266e6
Jose Roberto <jose.roberto.basanes@hotmail.com>
2020-01-20 16:55:30 -0300

    Dataservice

--- application/helpers/departamento_helper.php
+++ application/helpers/departamento_helper.php
@@ -1,12 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-if(!function_exists('dpto')){
-    function dpto($json)
+if(!function_exists('Dpto')){
+    function Dpto($json)
     {
         $array =  $json;
        // var_dump($array->menu);die;
         $html = '';
-        foreach ($array->dpto->dpto as $i) {
+        foreach ($array->Dpto->Dpto as $i) {
             $html .= '<option>'.$i->nombre.'</option>';
         }
         return $html;
