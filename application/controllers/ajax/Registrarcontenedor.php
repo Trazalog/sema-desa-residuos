80a25ccc8d1d6089c850a2f3281c0dcfecaf821a
Jose Roberto <jose.roberto.basanes@hotmail.com>
2020-02-05 16:10:16 -0300

    Fix

--- application/controllers/ajax/Registrarcontenedor.php
+++ application/controllers/ajax/Registrarcontenedor.php
@@ -4,12 +4,12 @@ class Registrarcontenedor  extends CI_Controller {
     function __construct()
     {
       parent::__construct();
-      $this->load->model('general/Registrarcontenedores');
+      $this->load->model('general/Estructura/Contenedor');
     }
     public function guardarDato()
     {
         $datos =  $this->input->post();
-        $resp = $this->Registrarcontenedores->guardarDatos($datos);
+        $resp = $this->Contenedor->guardarDatos($datos);
         if($resp){
           echo "ok";
         }else{
