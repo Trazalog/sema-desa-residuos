9379158226b080a6aacca76f5f319d8dd15ffdec
Jose Roberto <jose.roberto.basanes@hotmail.com>
2020-02-14 15:36:29 -0300

    Lista datatable

--- application/views/layout/Zonas/Lista_Circuitos.php
+++ application/views/layout/Zonas/Lista_Circuitos.php
@@ -17,33 +17,33 @@
 
     <tbody>
     <?php
-                    if($circuitos)
-                    {
-                        foreach($circuitos as $fila)
-                        {
-                        echo '<tr data-json:'.json_encode($fila).'>';
-                        echo    '<td>';
-                        echo    '<button type="button" title="Editar" class="btn btn-primary btn-circle" data-toggle="modal"
-                        data-target="#modalEdit"><span class="glyphicon glyphicon-pencil"
-                            aria-hidden="true"></span></button>&nbsp
-                    <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal"
-                        data-target="#modalInfo"><span class="glyphicon glyphicon-info-sign"
-                            aria-hidden="true"></span></button>&nbsp
-                    <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal"
-                        data-target="#modalPunto"><span class="glyphicon glyphicon-record"
-                            aria-hidden="true"></span></button>&nbsp
-                    <button type="button" title="eliminar" class="btn btn-primary btn-circle"><span
-                            class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>&nbsp';
-                            
-                        echo   '</td>';
-                        echo    '<td>'.$fila->codigo.'</td>';
-                        echo    '<td>'.$fila->chof_id.'</td>';
-                        echo    '<td>'.$fila->vehi_id.'</td>';
-                        echo    '<td>'.$fila->descripcion.'</td>';
-                        echo '</tr>';
-                    }
-                    }
-                    ?>
+        if($circuitos)
+        {
+            foreach($circuitos as $fila)
+            {
+            echo '<tr data-json:'.json_encode($fila).'>';
+            echo    '<td>';
+            echo    '<button type="button" title="Editar" class="btn btn-primary btn-circle" data-toggle="modal"
+            data-target="#modalEdit"><span class="glyphicon glyphicon-pencil"
+                aria-hidden="true"></span></button>&nbsp
+        <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal"
+            data-target="#modalInfo"><span class="glyphicon glyphicon-info-sign"
+                aria-hidden="true"></span></button>&nbsp
+        <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal"
+            data-target="#modalPunto"><span class="glyphicon glyphicon-record"
+                aria-hidden="true"></span></button>&nbsp
+        <button type="button" title="eliminar" class="btn btn-primary btn-circle"><span
+                class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>&nbsp';
+                
+            echo   '</td>';
+            echo    '<td>'.$fila->codigo.'</td>';
+            echo    '<td>'.$fila->chof_id.'</td>';
+            echo    '<td>'.$fila->vehi_id.'</td>';
+            echo    '<td>'.$fila->descripcion.'</td>';
+            echo '</tr>';
+        }
+        }
+        ?>
     </tbody>
 </table>
 
--- application/views/layout/Zonas/Lista_zona.php
+++ application/views/layout/Zonas/Lista_zona.php
@@ -1,49 +1,44 @@
-                    <!--__________________HEADER TABLA___________________________-->
-                    <table id="tabla_zonas" class="table table-bordered table-striped">
-                        <thead class="thead-dark" bgcolor="#eeeeee">
+    <!--__________________HEADER TABLA___________________________-->
+    <table id="tabla_zonas" class="table table-bordered table-striped">
+        <thead class="thead-dark" bgcolor="#eeeeee">
 
-                            <th>Acciones</th>
-                            <th>Nombre</th>
-                            <th>Departamento</th>
-                            <!--<th>Circuito</th>-->
-                            <th>Descripcion</th>
-                            
+            <th>Acciones</th>
+            <th>Nombre</th>
+            <th>Departamento</th>
+            <!--<th>Circuito</th>-->
+            <th>Descripcion</th>
+            
 
-                        </thead>
+        </thead>
 
-                        <!--__________________BODY TABLA___________________________-->
+    <!--__________________BODY TABLA___________________________-->
 
-                    <tbody>
-                    <?php
-                    if($zonas)
-                    {
-                        foreach($zonas as $fila)
-                        {
-                        echo '<tr data-json:'.json_encode($fila).'>';
-                        echo    '<td>';
-                        echo    '<button type="button" title="Editar" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#modalEdit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>&nbsp
-                            <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#modalInfo"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></button>&nbsp 
-                            <button type="button" title="eliminar" class="btn btn-primary btn-circle"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>&nbsp';
-                            
-                        echo   '</td>';
-                        echo    '<td>'.$fila->nombre.'</td>';
-                        echo    '<td>'.$fila->depa_nom.'</td>';
-                        //echo    '<td>circuito</td>';
-                        echo    '<td>'.$fila->descripción.'</td>';
-                        echo '</tr>';
-                    }
-                    }
-                    ?>
-                    </tbody>
-                        </table>
+    <tbody>
+    <?php
+    if($zonas)
+    {
+        foreach($zonas as $fila)
+        {
+        echo '<tr data-json:'.json_encode($fila).'>';
+        echo    '<td>';
+        echo    '<button type="button" title="Editar" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#modalEdit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>&nbsp
+            <button type="button" title="Info" class="btn btn-primary btn-circle" data-toggle="modal" data-target="#modalInfo"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></button>&nbsp 
+            <button type="button" title="eliminar" class="btn btn-primary btn-circle"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>&nbsp';
+            
+        echo   '</td>';
+        echo    '<td>'.$fila->nombre.'</td>';
+        echo    '<td>'.$fila->depa_nom.'</td>';
+        //echo    '<td>circuito</td>';
+        echo    '<td>'.$fila->descripción.'</td>';
+        echo '</tr>';
+    }
+    }
+    ?>
+    </tbody>
+        </table>
 
-                    <!--__________________FIN TABLA___________________________-->
+    <!--__________________FIN TABLA___________________________-->
 
-
-                    
 <script>
-
 DataTable($('#tabla_zonas'))
-
 </script>
-           
