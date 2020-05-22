993e5ea92a7bfa07e2d6e17d85d5eec7f3adcfae
checho <sledesma@trazalog.com>
2020-05-21 21:21:46 -0300

    Completando y arreglando Vehiculo

--- application/controllers/general/Estructura/Vehiculo.php
+++ application/controllers/general/Estructura/Vehiculo.php
@@ -28,8 +28,9 @@
         {
             log_message('INFO','#TRAZA|VEHICULO|templateVehiculos() >>');
             // $data[''] = $this->Infracciones->obtener_();
-
-            $this->load->view('layout/Vehiculos/registrar_vehiculo', $data);
+            $data["vehiculos"] = $this->Vehiculos->Listar_Vehiculo();
+            $data["transportista"] = $this->Vehiculos->Obtener_Transportista();
+            $this->load->view('layout/Vehiculos/registrar_vehiculo',$data);
         }
 
         /**
@@ -41,8 +42,8 @@
         {
             log_message('INFO','#TRAZA|VEHICULO|Guardar_Vehiculo() >>');
             $datos =  $this->input->post('datos');
-            $resp = $this->Vehiculos->Guardar_Vehiculo($datos);
-            if($resp){
+            $resp = $this->Vehiculos->Guardar_Vehiculos($datos);
+            if($resp == 1){
             echo "ok";
             }else{
             echo "error";
@@ -55,9 +56,55 @@
         * @return view lista_vehiculos
         */
         function Listar_Vehiculo()
-        {
+        {   
+            log_message('INFO','#TRAZA|Vehiculo|Listar_Vehiculo() >>');
             $data["vehiculos"] = $this->Vehiculos->Listar_Vehiculo();
             $this->load->view('layout/Vehiculos/lista_vehiculos',$data);
+        }
+
+        /**
+        * Elimina un vehiculo dado un id
+        * @param  string id vehiculo , 1 
+        * @return string "ok, error"
+        */
+        function Borrar_Vehiculo(){
+            log_message('INFO','#TRAZA|Vehiculo|Borrar_Vehiculo() >>');
+            $resp = $this->Vehiculos->Borrar_vehiculo($this->input->post('eliminar'));
+            if($resp == 1){
+                echo 'ok';
+            }else{
+                log_message('ERROR','#TRAZA|Contenedor|Borrar_Contenedor() >> $resp: '.$resp); 
+                echo 'error';
+            }
+        }
+
+        /**
+        * Actualiza un Vehiculo
+        * @param  string datos de vehiculo 
+        * @return string "ok, error"
+        */    
+        function Actualizar_Vehiculo(){
+            log_message('INFO','#TRAZA|Vehiculo|Actualizar_Vehiculo() >>'); 
+            $datos =  $this->input->post('vehiculo');
+            $resp = $this->Vehiculos->actualizar_Vehiculo($datos);
+            if($resp){
+                echo 'ok';
+            }else{
+            log_message('ERROR','#TRAZA|Vehiculo|Actualizar_Vehiculo() >> $resp: '.$resp);
+            echo 'error';
+            }
+
+        }
+
+         /**
+        * Obtiene transpostista para listar en el select
+        * @param  
+        * @return json transportistas
+        */  
+        function ObtenerTransportistas(){
+            log_message('INFO','#TRAZA|Vehiculo|ObtenerTransportistas() >>'); 
+            $dato["tran"]= $this->Vehiculos->Obtener_Transportista();
+            echo json_encode($dato);
         }
 
     }
