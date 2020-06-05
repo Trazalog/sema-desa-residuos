26a4de91ac169696ee1bcc1b33b440cf46a7c1fd
checho <sledesma@trazalog.com>
2020-06-05 01:12:55 -0300

    abm incidencias
    registrar incidencias

--- application/models/general/Estructura/Incidencias.php
+++ application/models/general/Estructura/Incidencias.php
@@ -15,11 +15,48 @@ class Incidencias extends CI_Model
         parent::__construct();
     }
 
-    public function guardarDatos($datos)
+    public function guardarIncidencias($datos)
     {
-      $aux = $this->rest->callAPI("POST","http://localhost:3000/tablaprocesoproductivo", $datos);
+      $post["incidencia"] = $datos;
+      $aux = $this->rest->callAPI("POST","/incidencias", $post);
       $aux =json_decode($aux["status"]);
       return $aux;
+    }
+    function ObtenerOT($nro)
+    {
+      $aux = $this->rest->callAPI("GET",REST."/ordenesTransporte/lista/$nro");
+      $aux =json_decode($aux["data"]);
+      return $aux->ordenTransporte->ordenesTransporte;
+    }
+
+    function getTipoResiduos()
+    {
+      log_message('INFO','#TRAZA|Contenedores|obtener_Estado() >> '); 
+      $aux = $this->rest->callAPI("GET",REST."/tablas/tipo_carga");
+      $aux =json_decode($aux["data"]);
+      return $aux->valores->valor;
+    }
+
+    function getDispFinal(){
+      log_message('INFO','#TRAZA|Contenedores|obtener_Estado() >> '); 
+      $aux = $this->rest->callAPI("GET",REST."/tablas/disposicion_final");
+      $aux =json_decode($aux["data"]);
+      return $aux->valores->valor;
+    }
+
+    function getIncidencia()
+    {
+      log_message('INFO','#TRAZA|Contenedores|obtener_Estado() >> '); 
+      $aux = $this->rest->callAPI("GET",REST."/tablas/tipos_incidencia");
+      $aux =json_decode($aux["data"]);
+      return $aux->valores->valor;
+    }
+
+    function ObtenerProximoID()
+    {
+      $aux = $this->rest->callAPI("GET",REST."/ordenTransporte/prox");
+      $aux =json_decode($aux["data"]);
+      return $aux->respuesta;
     }
 
 }
